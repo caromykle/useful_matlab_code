37516e011d7cbe56c82f5d1cb9265b5447b4f07e
Caroline Myklebust <caromykle@gmail.com>
2019-10-09 08:52:52 +0200

    Added the linear operator for a backward approximation of the first derivative in x.

---
+++ discretizations/Dx.m
@@ -0,0 +1,17 @@
+function [ du_x ] = Dx( u )
+% APPROXIMATING FIRST DERIVATIVE
+%-------------------------------
+% -Caroline Myklebust 08.10.2019
+% Constructing a linear operator matrix which returns a periodic, onesided
+% approximation of the first partial derivative in x of a input matrix u.
+
+    [m,n]=size(u);
+    h = (m-1)/(m-1);
+    A = speye(m*n);
+    B = sparse(m+1:m*n, 1:m*n-m,-1,m*n,m*n);
+    C = sparse(1:m,m*n-m+1:m*n,-1,m*n,m*n);
+    D=A+B+C;
+    du_x= reshape((1/h)*D*u(:),[m,n]);
+    
+    
+end
