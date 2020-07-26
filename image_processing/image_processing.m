4449f66eaf78f02fc9f3f0b372650f65e00b8aa3
Caroline Myklebust <caromykle@gmail.com>
2020-07-26 15:59:53 +0200

    updated image_processing file

--- image_processing/image_processing.m
+++ image_processing/image_processing.m
@@ -7,3 +7,18 @@ A = im2double(A);
 
 % Extracting one layer from the image
 A = A(:,:,1);
+
+% Retrieving the size of the image stored in variables m and n
+[m,n] = size(A);
+
+% OR reformat image to arbitrary size [p,q] as follows
+p = 200;
+q = 200;
+A = imresize(A,[p,q]);
+
+% More compact format:
+B = imread('your_image.png');
+B = im2double(imresize(B(:,:,1),[p,q]));
+
+
+
